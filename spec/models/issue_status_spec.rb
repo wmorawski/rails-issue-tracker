9718e13b5bc7f8c7c4be2b0445b8860073db4eaf
Wiktor Morawski <wiktor.morawski@recruitee.com>
2021-11-09 13:20:23 +0100

    test: remvoe seeder

--- spec/models/issue_status_spec.rb
+++ spec/models/issue_status_spec.rb
@@ -1,5 +1,11 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe IssueStatus, type: :model do
+  describe "#validations" do
+    it "does smth" do
+      pp described_class.all
+    end
+  end
+
   pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,9 +33,6 @@ end
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 
-  config.before(:suite) do
-    Rails.application.load_seed
-  end
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
